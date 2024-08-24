33ae4c770188e62e57221101a14e98db38db7174
Mark Mwima <markmwimar@gmail.com>
2024-08-24 23:26:27 +0300

    tomcat instance

--- src/main/java/com/mwimar/App.java
+++ src/main/java/com/mwimar/App.java
@@ -1,9 +1,13 @@
 package com.mwimar;
 
-public class App 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hey YAll!" );
+        Tomcat tomcat = new Tomcat();
+        tomcat.start();
     }
 }
